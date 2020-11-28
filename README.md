7e833330678e1574ba5d7ed2ed8cd623f1790562
Giannikis <steliosgiannik@gmail.com>
2020-11-28 14:46:20 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Customer Churn - Engineering
+
+The main purpose of this project, is to focus on the engineering 
+part and not so on the modelling part. We will create efficient data pipelines and
+will adhere to coding best practices using different tools, languages, and technologies like
+Python, Scala, Spark, Docker, CI/CD tools, etc.
+
+This repo will be used for testing different technologies.
+
+## Getting Started
+
+TBC
+
+### Prerequisites
+
+TBC
+
+
+### Installing
+
+TBC
+
+## Built With
+
+TBC
+
---
+++ src/python/src/config/config.py
@@ -0,0 +1,20 @@
+CAT_VARS = ['MultipleLines',
+            'InternetService',
+            'OnlineSecurity',
+            'OnlineBackup',
+            'DeviceProtection',
+            'TechSupport',
+            'StreamingTV',
+            'StreamingMovies',
+            'Contract',
+            'PaymentMethod']
+
+NUMERICAL_VARS = ['tenure', 'MonthlyCharges', 'TotalCharges']
+
+FEATURES = ['gender', 'SeniorCitizen', 'Partner', 'Dependents',
+            'tenure', 'PhoneService', 'MultipleLines', 'InternetService',
+            'OnlineSecurity', 'OnlineBackup', 'DeviceProtection', 'TechSupport',
+            'StreamingTV', 'StreamingMovies', 'Contract', 'PaperlessBilling',
+            'PaymentMethod', 'MonthlyCharges', 'TotalCharges']
+
+TARGET = 'Churn'
