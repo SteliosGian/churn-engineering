e74de71349d66d87b932604042fb92c17f9b786a
Giannikis <steliosgiannik@gmail.com>
2020-11-29 02:22:37 +0000

    Start testing

---
+++ src/python/tests/test_preprocessor.py
@@ -0,0 +1,27 @@
+import sys
+sys.path.insert(0, 'src')
+import pytest
+from preprocessing import preprocessor
+
+def test_fit_and_save_encodings():
+    pass
+
+
+def test_load_and_transform_encoder():
+    pass
+
+
+def test_load_and_invert_transform_encoder():
+    pass
+
+
+def test_convert_empty_lines_to_na():
+    pass
+
+
+def test_remove_na():
+    pass
+
+
+def test_encode_target():
+    pass
