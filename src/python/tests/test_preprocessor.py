7a94f25719c3435b83fea1f0adf7a45d5fc78ebb
Giannikis <steliosgiannik@gmail.com>
2020-11-29 15:01:16 +0000

    Test save encodings

--- src/python/tests/test_preprocessor.py
+++ src/python/tests/test_preprocessor.py
@@ -1,10 +1,33 @@
 import sys
 sys.path.insert(0, 'src')
 import pytest
-from preprocessing import preprocessor
+import os.path
+import pandas as pd
+from preprocessing import preprocessor as pp
+from config import config
+#os.path.isfile(fname)
 
-def test_fit_and_save_encodings():
-    pass
+
+@pytest.fixture()
+def get_data():
+    return pd.read_csv('tests/resources/sample_df.csv')
+
+
+@pytest.fixture(scope="session", autouse=True)
+def cleanup(request):
+    """Cleanup testing files once we are finished."""
+    def remove_test_files(test_encodings_path='tests/encodings/'):
+        files = os.listdir(test_encodings_path)
+        for col in config.CAT_VARS:
+            if f"{col}_encoder.npy" in files:
+                os.remove(f"{test_encodings_path}{col}_encoder.npy")
+    request.addfinalizer(remove_test_files)
+
+
+def test_fit_and_save_encodings(get_data, test_encodings_path='tests/encodings'):
+    pp.fit_and_save_encodings(get_data, cat_cols=config.CAT_VARS, encodings_path=test_encodings_path)
+    for var in config.CAT_VARS:
+        assert os.path.isfile(f"{test_encodings_path}/{var}_encoder.npy"), "Encodings not found"
 
 
 def test_load_and_transform_encoder():
