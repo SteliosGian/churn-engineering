fcde03bafc4911cb8b42a1132d53e4ca648e95bd
Giannikis <steliosgiannik@gmail.com>
2020-12-07 22:07:01 +0000

    Complete preprocessor tests

--- src/python/tests/test_preprocessor.py
+++ src/python/tests/test_preprocessor.py
@@ -3,9 +3,10 @@ sys.path.insert(0, 'src')
 import pytest
 import os.path
 import pandas as pd
+import pandas.api.types as ptypes
 from preprocessing import preprocessor as pp
 from config import config
-#os.path.isfile(fname)
+
 
 
 @pytest.fixture()
@@ -30,21 +31,32 @@ def test_fit_and_save_encodings(get_data, test_encodings_path='tests/encodings')
         assert os.path.isfile(f"{test_encodings_path}/{var}_encoder.npy"), "Encodings not found"
 
 
-def test_load_and_transform_encoder():
-    pass
+def test_load_and_transform_encoder(get_data, test_encodings_path='tests/encodings'):
+    pp.fit_and_save_encodings(get_data, cat_cols=config.CAT_VARS, encodings_path=test_encodings_path)
+    df = pp.load_and_transform_encoder(get_data, cat_cols=config.CAT_VARS, encodings_path=test_encodings_path)
+    assert all(ptypes.is_numeric_dtype(df[col]) for col in config.CAT_VARS), "Data types of CAT_VARS must be numeric"
 
 
-def test_load_and_invert_transform_encoder():
-    pass
+def test_load_and_invert_transform_encoder(get_data, test_encodings_path='tests/encodings'):
+    pp.fit_and_save_encodings(get_data, cat_cols=config.CAT_VARS, encodings_path=test_encodings_path)
+    df = pp.load_and_transform_encoder(get_data, cat_cols=config.CAT_VARS, encodings_path=test_encodings_path)
+    df = pp.load_and_invert_transform_encoder(df, cat_cols=config.CAT_VARS, encodings_path=test_encodings_path)
+    assert all(ptypes.is_object_dtype(df[col])
+               for col in config.CAT_VARS
+               if df[[col]].columns != 'SeniorCitizen'), "Data types of CAT_VARS must be objects"
 
 
-def test_convert_empty_lines_to_na():
-    pass
+def test_convert_empty_lines_to_na(get_data, col=['TotalCharges']):
+    df = pp.convert_empty_lines_to_na(get_data, cols=col)
+    assert df[col].isnull().values.any(), f"{col} should have null values"
 
 
-def test_remove_na():
-    pass
+def test_remove_na(get_data, col=['TotalCharges']):
+    df = pp.convert_empty_lines_to_na(get_data, cols=col)
+    df = pp.remove_na(df)
+    assert df.notnull().values.all(), "Null values should not exist"
 
 
-def test_encode_target():
-    pass
+def test_encode_target(get_data):
+    df = pp.encode_target(get_data, target=config.TARGET)
+    assert ptypes.is_bool_dtype(df[config.TARGET]), "Target variable is not bool type"
