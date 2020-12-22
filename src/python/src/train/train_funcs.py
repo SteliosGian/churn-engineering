f1a841978be45bac5de2405d85051e6f9408460d
Giannikis <steliosgiannik@gmail.com>
2020-12-22 01:24:24 +0000

    Add docstrings to new functions

--- src/python/src/train/train_funcs.py
+++ src/python/src/train/train_funcs.py
@@ -9,11 +9,26 @@ from models.logistic_model import LogisticModel
 
 
 def model_create(params: dict):
+    """
+    Create the Logistic Regression model.
+    :param params: Params dictionary
+    :return: Model (LogisticModel)
+    """
     model = LogisticModel(**params)
     return model
 
 
 def model_evaluate(df: pd.DataFrame, features: list, target: str, metrics: dict, model, cv_rounds: int = 5):
+    """
+    Evaluate the model's performance with cross-validation.
+    :param df: Dataframe
+    :param features: Features list
+    :param target: Target variable
+    :param metrics: Metrics dictionary
+    :param model: Model from 'model_create" function
+    :param cv_rounds: Cross-validation rounds
+    :return: Scores, probability predictions, and binary predictions
+    """
     df = df.copy()
     scores = cross_validate(estimator=model.model,
                             X=df[features],
@@ -23,10 +38,10 @@ def model_evaluate(df: pd.DataFrame, features: list, target: str, metrics: dict,
                             return_train_score=True)
 
     preds_proba = cross_val_predict(estimator=model.model,
-                              X=df[features],
-                              y=df[target],
-                              cv=cv_rounds,
-                              method="predict_proba")[:, 1]
+                                    X=df[features],
+                                    y=df[target],
+                                    cv=cv_rounds,
+                                    method="predict_proba")[:, 1]
     preds = cross_val_predict(estimator=model.model,
                               X=df[features],
                               y=df[target],
@@ -36,22 +51,45 @@ def model_evaluate(df: pd.DataFrame, features: list, target: str, metrics: dict,
 
 
 def model_train(model, df: pd.DataFrame, features: list, target: str, path: str = None) -> None:
+    """
+    Train and save the model.
+    :param model: Model from 'model_create' function
+    :param df: Dataframe
+    :param features: Features list
+    :param target: Target variable
+    :param path: Path to save the model
+    :return: None
+    """
     df = df.copy()
     model.fit(df[features], df[target])
     model.save_model(path)
 
 
 def plot_prob_hist(predictions: np.ndarray, plot_path: str) -> None:
+    """
+    Saves the histogram of the predicted probabilities.
+    :param predictions: Predictions array
+    :param plot_path: Path to save the plot
+    :return: None
+    """
     sns.histplot(predictions, kde=True)
     plt.savefig(f'{plot_path}/probs_histogram.png', dpi=400)
 
 
 def plot_conf_matrix(df: pd.DataFrame, target: str, predictions: np.ndarray, plot_path: str) -> None:
+    """
+    Saves the confusion matrix
+    :param df: Dataframe
+    :param target: Target variable
+    :param predictions: Predictions array
+    :param plot_path: Path to save the plot
+    :return: None
+    """
     df = df.copy()
     conf_mat = confusion_matrix(df[target], predictions)
     df_cm = pd.DataFrame(conf_mat)
     sns.set(font_scale=1.4)
-    sns.heatmap(df_cm/np.sum(df_cm), annot=True,
+    sns.heatmap(df_cm / np.sum(df_cm), annot=True,
                 fmt='.2%', cmap='Blues')
     plt.xlabel('Predicted label')
     plt.ylabel('True label')
@@ -60,6 +98,16 @@ def plot_conf_matrix(df: pd.DataFrame, target: str, predictions: np.ndarray, plo
 
 def log_metrics(metrics: dict, params: dict, tracking_uri: str,
                 plot_path: str, models_path: str, round_dec: int = 3) -> None:
+    """
+    Logs model metrics, hyperparameters, plots, and model instance.
+    :param metrics: Metrics dictionary
+    :param params: Parameter dictionary to use
+    :param tracking_uri: The tracking uri. If local, it is the tracking folder to use
+    :param plot_path: Path of the saved plots
+    :param models_path: Path of the saved trained model
+    :param round_dec: Number of decimals to round the float numbers
+    :return: None
+    """
     mlflow.set_tracking_uri(tracking_uri)
     mlflow.set_experiment("Test experiment")
     with mlflow.start_run():
